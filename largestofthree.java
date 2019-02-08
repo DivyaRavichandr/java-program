8abec04062b86ffba4e8f5651376f4ac43c13cff
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-08 15:17:06 +0530

    Create largestofthree.java

---
+++ largestofthree.java
@@ -0,0 +1,19 @@
+class largestofthree{
+public static void main(string[] args){
+int a;
+int b;
+int c;
+System.out.print("Enter three numbers");
+Scanner scan =new Scanner(System.in);
+int num=scan nextInt();
+int num=scan nextInt();
+int num=scan nextInt();
+if(a>b && a>c)
+System.out.print("The number a is greatest");
+else if(b>a && b>c)
+System.out.print("The number b is greatest");
+else
+System.out.print("The number c is greatest");
+}
+}
+
