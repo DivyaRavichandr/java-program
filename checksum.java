13d5b775deffeaa597a1739b65677e07a85ca826
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-08 13:57:12 +0530

    Create checksum.java

---
+++ checksum.java
@@ -0,0 +1,13 @@
+class checksum{
+public static void main(string[] args){
+int a;
+Scanner scan =new Scanner(System.in);
+a=scan nextInt();
+if(a<0)
+System.out.print("The number is negative");
+else if(a>0)
+System.out.print("The number is postive");
+else
+System.out.print("The number is zero");
+}
+}
