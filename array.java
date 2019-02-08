f78da242347a442a872484eaed0c7bc66c64d792
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-08 16:06:48 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,18 @@
+class array{
+public static void main(string[] args){
+int sum=0;
+System.out.print("enter how many numbers to be added in array");
+Scanner scan =new Scanner(System.in);
+N=scan nextInt();
+int numberarray[];
+numberarray=new int[N];
+System.out.print("enter how many numbers to be added from array");
+Scanner scan =new Scanner(System.in);
+K=scan nextInt();
+for i in xrange(K)
+sum=sum+num;
+print(sum)
+System.out.print("The sum of first K is:",sum);
+}
+}
+
