1a647a6ddde143ddcf5a0fd2caf2c60f44158650
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-08 15:23:07 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,12 @@
+class leapyear{
+public static void main(string[] args){
+int a;
+System.out.print("Enter a year");
+Scanner scan =new Scanner(System.in);
+a=scan nextInt();
+f(a%2==0)
+System.out.print("The year is leapyear")
+else
+System.out.print("Not a leapyear");
+}
+}
