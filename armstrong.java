fb88b7190425270734e4cfa1a20ba6013e13a867
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-09 23:00:08 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -1,6 +1,8 @@
 import java.util.Scanner{
 class armstrong{
 public static void main(String args[]){
+Scanner scan=new Scanner(System.in);
+a=scan.nextInt();
 int num, a,total=0;
 while(num>0){
 a=num%10;
