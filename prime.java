9734f4f2e3d7a043e6955efa49032cc92d6fb88a
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-10 11:29:34 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 class prime{
 public static void main(String args[]){
-int n,count,i;
+int n,count=0,i;
 Scanner scan=new Scanner(System.in);
 n=scan.nextInt();
-for(i=2,i<=n,i++){
+for(i=2;i<=n;i++){
 if(n%2==0){
-count++
+count++;
 break;
 }
 }
 if(count==0)
 System.out.print("yes");
 else
-System.out.print("no")
+System.out.print("no");
 }
 }
