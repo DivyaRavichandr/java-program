34c45156bb571fb0cbd7bf8da33b1f7c9ca6734a
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-11 21:47:03 +0530

    Update checksum.java

--- checksum.java
+++ checksum.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 class checksum{
-public static void main(string[] args){
+public static void main(String[] args){
 int a;
 Scanner scan =new Scanner(System.in);
-a=scan nextInt();
+a=scan.nextInt();
 if(a<0)
 System.out.print("The number is negative");
 else if(a>0)
