2b9daef58bff6082545590666340bc71dc1257a8
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-02-12 07:20:21 +0530

    Update numberofdigits.java

--- numberofdigits.java
+++ numberofdigits.java
@@ -1,13 +1,13 @@
+import java. util. Scanner; 
 class numberofdigits{
-public static void main(string[] args){
+public static void main(String[] args){
 int count=0;
-System.out.print("enter an integer");
 Scanner scan =new Scanner(System.in);
-a=scan nextInt();
+int a=scan. nextInt();
 while(a>0){
 a=a/10;
 ++count;
-print(count);
-System.out.print("no.of digits in integer is:",count);
+}
+System.out.print(+count);
 }
 }
