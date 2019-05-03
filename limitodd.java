4d2595c1c1ba21dc0ba7d1fe4970c364ff57f816
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-05-03 09:26:44 +0530

    Update limitodd.java

--- limitodd.java
+++ limitodd.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 class limitodd{
 public static void main(String args[]){
-int m,n;
-Scanner scan=new Scanner(System.in);
-m=scan.nextInt();
-n=scan.nextInt();
-for i in range(m,n+1){
-while(i%2!==0){
-System.out.println(i);
-i++;
+	int m,n,i;
+	Scanner scan=new Scanner(System.in);
+	m=scan.nextInt();
+	n=scan.nextInt();
+	for(i=m;i<=n;i++){
+		if(i%2!=0){
+			System.out.println(i);
+			
+		}
+		else{
+			System.out.print(" ");
+			
+		}
+		
+	}
 }
-}
-}
+	
+
 }
